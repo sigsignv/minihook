b48f3389145a05af02de0004395a78f4fae48e4a
Sigsign <sig@signote.cc>
2023-05-04 02:34:06 +0900

    Split to queryEntries()

--- client.go
+++ client.go
@@ -11,20 +11,18 @@ type MinifluxClient struct {
 	Token  string
 }
 
-func (c *MinifluxClient) LatestEntryID() (int, error) {
-	client := miniflux.New(c.Server, c.Token)
+func (c *MinifluxClient) LatestEntryID() (int64, error) {
 	filter := &miniflux.Filter{
 		Order:     "id",
 		Direction: "desc",
 		Limit:     1,
 	}
-	r, err := client.Entries(filter)
+	entries, err := c.queryEntries(filter)
 	if err != nil {
 		return -1, err
 	}
-	entries := r.Entries
 	for _, entry := range entries {
-		return int(entry.ID), nil
+		return entry.ID, nil
 	}
 	return -1, fmt.Errorf("miniflux has no entry")
 }
@@ -33,4 +31,13 @@ func (c *MinifluxClient) Verify() error {
 	client := miniflux.New(c.Server, c.Token)
 	_, err := client.Me()
 	return err
+}
+
+func (c *MinifluxClient) queryEntries(filter *miniflux.Filter) (miniflux.Entries, error) {
+	client := miniflux.New(c.Server, c.Token)
+	r, err := client.Entries(filter)
+	if err != nil {
+		return nil, err
+	}
+	return r.Entries, nil
 }
